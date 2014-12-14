4548cc5e8df024fe86bcd6473e26f3f785c5b718
Alexander Hosking <alexander@ahosking.com>
2014-12-14 02:16:46 -0500

    Time addition is mostly whole now.

--- pytrack.py
+++ pytrack.py
@@ -1,4 +1,5 @@
 import requests
+import time #Required for time time stamping in youtrack
 #This class will allow you to submit to a specified Youtrack installation
 #via python and the requests module.
 
@@ -10,7 +11,7 @@ class pytrack(object):
         self.password = password
         if self.port != None:
             #Port has been entered, affix it to the base URL
-            self.baseURL += ":" + self.port
+            self.baseURL += ":" + self.port + "/rest/"
 
     def add_comment(self, issue, author, comment):
         '''
@@ -26,21 +27,30 @@ class pytrack(object):
         # outrack_url = "http://youtrack.arcestra.com:8180/rest/" + youtrack_method
         try:
             submitURL = self.baseURL +\
-                "/rest/" + "issue/" + issue + "/execute?comment=" + comment + "&runas=" + author
+                "issue/" + issue + "/execute?comment=" + comment + "&runas=" + author
             r = requests.post(submitURL, auth=(self.username, self.password))
             return ("Your comment was added to " + issue + " successfully.")
         except:
             return submitURL
 
-    def add_time(self, issue, time, description=None):
+    def add_time(self, issue, timeadded, description=None):
         """
         :param issue: The issue number
-        :param time: Time in a number with data for value (ex: H=hours, m=minutes)
+        :param time: Time in a number with data for value, this must be converted to minutes (ex: H=hours, m=minutes)
         :param description: Any text that should be added to the time entry
         :return: a success message with the issue, time added and text returned, else false
         """
+        workitem = "<?xml version = \"1.0\" encoding=\"UTF-8\"?>\n<workItem>\n" +\
+            "<date>" + str(int(time.time())) + "000" + "</date>\n" +\
+            "<duration>" + "60" + "</duration>\n" +\
+            "<description>" + "TIME!!!" + "</description>\n" +\
+            "</workItem>"
+        #Youtrack represents the correct date only when I add "000" to the end of the time stamp...
         try:
-            if description != None:
-                submitURL = self.baseURL
+            submitURL = self.baseURL + "issue/" + issue + "/timetracking/workitem"
+            headers = {'content-type': 'application/xml'}
+            r = requests.post(submitURL, data=workitem, headers=headers, auth=(self.username, self.password))
+            print r.status_code
+            return "Your time item was added successfully."
         except:
             return "Time addition failed."
