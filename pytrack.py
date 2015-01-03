d188f5b86184d4184c656e2ddcd910ee348a8705
Alexander Hosking <alexander@ahosking.com>
2015-01-02 22:02:17 -0500

    Added License File from GitHub

--- pytrack.py
+++ pytrack.py
@@ -1,5 +1,11 @@
-import requests
+#Standard classes
 import time #Required for time time stamping in youtrack
+
+#Dependencies
+import requests
+from xml.dom import minidom
+import xml.etree.ElementTree as ET
+
 #This class will allow you to submit to a specified Youtrack installation
 #via python and the requests module.
 
@@ -33,6 +39,36 @@ class pytrack(object):
         except:
             return submitURL
 
+
+
+    ### Time Tracking ###
+    def get_time(self, issue):
+        """
+        This will get all available time items on a given issue.
+        :param issue: The issue number to look up
+        :return: return a class of time items that each contain: id, date, duration, description and author
+        """
+        #Submit the request
+        submitURL = self.baseURL + "issue/" + issue + "/timetracking/workitem"
+        r = requests.get(submitURL, auth=(self.username, self.password))
+        #Parse the text and output it nicely:
+        tree = ET.fromstring(r.text)
+        print "------------------------------------"
+        print "There are ", str(len(tree)), "work items in", issue
+        print "------------------------------------"
+        for x in tree.iter("workItem"):
+            # print x.tag, x.attrib
+            try:
+                for a in x.iter():
+                    if not a.tag == "workItem":
+                        if a.attrib == {}:
+                            print a.tag, a.text
+                        else:
+                            print a.tag, a.attrib, a.text
+            except:
+                pass
+
+            
     def add_time(self, issue, timeadded, description=None):
         """
         :param issue: The issue number
@@ -51,6 +87,24 @@ class pytrack(object):
             headers = {'content-type': 'application/xml'}
             r = requests.post(submitURL, data=workitem, headers=headers, auth=(self.username, self.password))
             print r.status_code
+            ##FINISH this message
             return "Your time item was added successfully."
         except:
-            return "Time addition failed."
+            return
+
+    def delete_time(self, issue, itemID):
+        pass
+
+    ### Projects ###
+    def get_projects(self, verbose=None):
+        '''
+        :param verbose:
+        :return: return a dictionary of projects that are currently available
+        '''
+        if verbose != True:
+            verbose = False
+        submitURL = self.baseURL + "project/all?" + str(verbose)
+
+        r = requests.get(submitURL, auth=(self.username, self.password))
+        print r.status_code
+        return r.text
